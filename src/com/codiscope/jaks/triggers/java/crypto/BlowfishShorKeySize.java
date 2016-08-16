b4cacaad537910062cb09a76d9ac1e6b1cd6b4eb
sanjay <sanjay.singh@3pillarglobal.com>
2016-08-16 22:31:12 +0530

    Re-factor test code, added strong key method

--- src/com/codiscope/jaks/triggers/java/crypto/BlowfishShorKeySize.java
+++ src/com/codiscope/jaks/triggers/java/crypto/BlowfishShorKeySize.java
@@ -1,5 +1,6 @@
 package com.codiscope.jaks.triggers.java.crypto;
 
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -8,27 +9,41 @@ import javax.crypto.SecretKey;
 
 public class BlowfishShorKeySize {
 
+	public Key strongKeySize() throws NoSuchAlgorithmException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
+		// Now set the keysize to 256 bits
+		keyGenerator.init(256);
+		Key key = keyGenerator.generateKey();
+		return key;
+	}
+
+	public Key weakKeySize() throws NoSuchAlgorithmException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
+		// Now set the keysize to 32 bits
+		keyGenerator.init(32);
+		Key key = keyGenerator.generateKey();
+		return key;
+	}
+
 	public SecretKey weakKeySize1() throws NoSuchAlgorithmException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
-        keyGen.init(64);
+		KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
+		keyGen.init(64);
+		SecretKey key = keyGen.generateKey();
+		return key;
+	}
 
-        SecretKey key = keyGen.generateKey();
-        return key;
-    }
+	public SecretKey weakKeySize2() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
+		keyGen.init(96, new SecureRandom());
+		SecretKey key = keyGen.generateKey();
+		return key;
+	}
 
-    public SecretKey weakKeySize2() throws NoSuchAlgorithmException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
-        keyGen.init(96, new SecureRandom());
+	public SecretKey okKeySize1() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
+		keyGen.init(128, new SecureRandom());
 
-        SecretKey key = keyGen.generateKey();
-        return key;
-    }
-
-    public SecretKey okKeySize1() throws NoSuchAlgorithmException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
-        keyGen.init(128, new SecureRandom());
-
-        SecretKey key = keyGen.generateKey();
-        return key;
-    }
+		SecretKey key = keyGen.generateKey();
+		return key;
+	}
 }
