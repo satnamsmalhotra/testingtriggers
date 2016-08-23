aa7474f65c88bddf62e092231b1981d699b98d0f
aleh.haidash <aleh.haidash@instinctools.ru>
2016-08-23 13:17:59 +0300

    Changed cipher integrity test

--- src/com/codiscope/jaks/triggers/java/crypto/CipherIntegrity.java
+++ src/com/codiscope/jaks/triggers/java/crypto/CipherIntegrity.java
@@ -6,11 +6,15 @@ import java.security.NoSuchAlgorithmException;
 
 public class CipherIntegrity {
 
-    public void cipherIntegrity() throws NoSuchPaddingException, NoSuchAlgorithmException {
+    public void positiveTest() throws NoSuchPaddingException, NoSuchAlgorithmException {
         Cipher cipher = Cipher.getInstance("AAA/ECB/AAA");
-        Cipher cipher1 = Cipher.getInstance("AAA/CBC/AAA");
-        Cipher cipher2 = Cipher.getInstance("AAA/OFB/AAA");
-        Cipher cipher3 = Cipher.getInstance("AAA/CTR/AAA");
+        cipher = Cipher.getInstance("AAA/CBC/AAA");
+        cipher = Cipher.getInstance("AAA/OFB/AAA");
+        cipher = Cipher.getInstance("AAA/CTR/AAA");
 
+    }
+
+    public void negativeTest() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("AAA/GCM/AAA");
     }
 }
