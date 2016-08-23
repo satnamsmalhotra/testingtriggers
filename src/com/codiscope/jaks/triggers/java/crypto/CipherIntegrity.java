90cb4f52cf6d6734e38713f0b4e51137ed9f2959
aleh.haidash <aleh.haidash@instinctools.ru>
2016-08-23 15:56:23 +0300

    Made some code refactoring.

--- src/com/codiscope/jaks/triggers/java/crypto/CipherIntegrity.java
+++ src/com/codiscope/jaks/triggers/java/crypto/CipherIntegrity.java
@@ -4,8 +4,14 @@ import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * The class CipherIntegrity
+ */
 public class CipherIntegrity {
 
+    /**
+     * Test which should be found by Jacks
+     */
     public void positiveTest() throws NoSuchPaddingException, NoSuchAlgorithmException {
         Cipher cipher = Cipher.getInstance("AAA/ECB/AAA");
         cipher = Cipher.getInstance("AAA/CBC/AAA");
@@ -14,6 +20,9 @@ public class CipherIntegrity {
 
     }
 
+    /**
+     * Test which shouldn't be found by Jacks
+     */
     public void negativeTest() throws NoSuchPaddingException, NoSuchAlgorithmException {
         Cipher cipher = Cipher.getInstance("AAA/GCM/AAA");
     }
--- src/com/codiscope/jaks/triggers/java/hibr/HibernateSqlI.java
+++ src/com/codiscope/jaks/triggers/java/hibr/HibernateSqlI.java
@@ -4,14 +4,21 @@ import org.hibernate.Query;
 import org.hibernate.classic.Session;
 import tests.sources.PrivateSource;
 
+import java.util.List;
+
+/**
+ * The class HibernateSqlI
+ */
 public  class HibernateSqlI {
 
     private PrivateSource privateSource = new PrivateSource();
-
-    private void createQuery(){
+    /**
+     * Test which should be found by Jacks
+     */
+    private void  positiveTest(){
 
         String source = privateSource.method1();
-        Session session = null;
+        Session session = getSession();
         session.createFilter(source, source);
         session.delete(source);
         session.delete(source, source);
@@ -21,11 +28,18 @@ public  class HibernateSqlI {
         session.find(source);
     }
 
-
-    private void createFilter(){
+    /**
+     * Test which shouldn't be found by Jacks
+     */
+    public void negativeTest(){
+        Session session = getSession();
         String source = privateSource.method1();
-        Session session =null;
-        session.createFilter(source,source);
+        Query q = session.createQuery("SELECT order.name FROM Order AS order WHERE order.orderID = '" + source + "'");
+        List results = q.list();
+
     }
 
+    private Session getSession(){
+        return null;
+    }
 }
