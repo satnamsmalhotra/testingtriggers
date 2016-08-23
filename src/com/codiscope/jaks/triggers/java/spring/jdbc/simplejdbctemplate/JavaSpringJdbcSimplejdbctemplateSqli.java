f670a856320e154e868df9bb536491a88f0c80a6
Shelden93 <aliaksandr.zhvirblia@instinctools.ru>
2016-08-23 13:02:07 +0300

    Implemented new tests

--- src/com/codiscope/jaks/triggers/java/spring/jdbc/simplejdbctemplate/JavaSpringJdbcSimplejdbctemplateSqli.java
+++ src/com/codiscope/jaks/triggers/java/spring/jdbc/simplejdbctemplate/JavaSpringJdbcSimplejdbctemplateSqli.java
@@ -1,5 +1,7 @@
 package com.codiscope.jaks.triggers.java.spring.jdbc.simplejdbctemplate;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.jdbc.datasource.DelegatingDataSource;
@@ -7,10 +9,22 @@ import org.springframework.jdbc.datasource.DelegatingDataSource;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import tests.sources.DatabaseSource;
+import tests.sources.FileSource;
+import tests.sources.PrivateSource;
+import tests.sources.WebSource;
+import tests.sources.WebSourceCookie;
+
 /**
  * The Class JavaSpringJdbcSimplejdbctemplateSqli
  */
 public class JavaSpringJdbcSimplejdbctemplateSqli {
+
+    private WebSourceCookie webcookie = new WebSourceCookie();
+    private DatabaseSource databasesource = new DatabaseSource();
+    private FileSource filesource = new FileSource();
+    private PrivateSource privatesource = new PrivateSource();
+    private WebSource websource = new WebSource();
 
     public void possessiveTest() {
 
@@ -25,5 +39,14 @@ public class JavaSpringJdbcSimplejdbctemplateSqli {
         SimpleJdbcTemplate jdbcTemplate = new SimpleJdbcTemplate(new DelegatingDataSource());
         jdbcTemplate.queryForMap("", new HashMap<String, Object>());
         jdbcTemplate.queryForList("", new ArrayList<String>());
+    }
+
+    public void test() {
+        JdbcTemplate template = new JdbcTemplate();
+
+        ResultSetExtractor rse = null;
+
+        template.update(databasesource.method1());
+        template.query(websource.method1(), rse);
     }
 }
--- src/com/codiscope/jaks/triggers/java/spring/mail/SpringMailForging.java
+++ src/com/codiscope/jaks/triggers/java/spring/mail/SpringMailForging.java
@@ -4,10 +4,22 @@ import org.springframework.mail.MailMessage;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
+import tests.sources.DatabaseSource;
+import tests.sources.FileSource;
+import tests.sources.PrivateSource;
+import tests.sources.WebSource;
+import tests.sources.WebSourceCookie;
+
 /**
  * The Class SpringMailForging
  */
 public class SpringMailForging {
+
+    private DatabaseSource databasesource = new DatabaseSource();
+    private FileSource filesource = new FileSource();
+    private PrivateSource privatesource = new PrivateSource();
+    private WebSource websource = new WebSource();
+    private WebSourceCookie webcookie = new WebSourceCookie();
 
     private MailSender mailSender;
 
@@ -36,5 +48,24 @@ public class SpringMailForging {
         final String text = message.getText();
 
         mailSender.send(message);
+    }
+
+    public void testSpringSimpleMailMesssage() {
+        //Test SimpleMailMesssage when source websource
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setSubject(websource.method2());
+
+        //Test SimpleMailMesssage when source DatabaseSource
+        mailMessage.setFrom(databasesource.method2());
+        mailMessage.setCc(databasesource.method1());
+
+        //Test SimpleMailMesssage when source WebCookie
+        mailMessage.setBcc(webcookie.method5());
+
+        //Test SimpleMailMesssage when source FileDource
+        mailMessage.setText(filesource.method1());
+
+        //Test SimpleMailMesssage when source PrivateDource
+        mailMessage.setText(privatesource.method1());
     }
 }
