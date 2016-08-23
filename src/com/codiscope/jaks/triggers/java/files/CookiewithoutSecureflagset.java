94ae99c1e4767f5894b5ec2459bd68dbf065000a
Satnam Malhotra <satnam.malhotra@3pillar.corp>
2016-08-23 13:36:44 +0530

    Trigger Code

---
+++ src/com/codiscope/jaks/triggers/java/files/CookiewithoutSecureflagset.java
@@ -0,0 +1,20 @@
+package com.codiscope.jaks.triggers.java.files;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookiewithoutSecureflagset {
+	
+	
+	public void sendResponseWithFlag(HttpServletRequest request, HttpServletResponse response){
+		String sessionid = request.getSession().getId();
+		response.setHeader("SET-COOKIE", "JSESSIONID=" + sessionid + "; secure");
+	}
+	
+	public void sendResponseWithoutFlag(HttpServletRequest request, HttpServletResponse response){
+		String sessionid = request.getSession().getId();
+		response.setHeader("SET-COOKIE", "JSESSIONID=" + sessionid + "; false");
+	}
+	
+
+}
