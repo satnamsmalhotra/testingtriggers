2e5a3fd171510e1282ed141ffc9ba14575e6896d
aleh.haidash <aleh.haidash@instinctools.ru>
2016-08-23 12:36:37 +0300

    Changed NullCipher test

--- src/com/codiscope/jaks/triggers/java/crypto/NullCipherTest.java
+++ src/com/codiscope/jaks/triggers/java/crypto/NullCipherTest.java
@@ -1,11 +1,22 @@
 package com.codiscope.jaks.triggers.java.crypto;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.NullCipher;
+import java.security.NoSuchAlgorithmException;
 
 public class NullCipherTest {
 
-    public void nullCipher(){
-        NullCipher cipher = null;
+    public void positiveTest(){
+        NullCipher cipher = createNullCipher();
+    }
+
+    public void  negativeTest() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding");
+    }
+
+    private NullCipher createNullCipher(){
+        return null;
     }
 
 }
